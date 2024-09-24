6f23975ee450e538eb5477b5b2844de29f4e21da
Schnitzel5 <tangphilip3@gmail.com>
2024-09-24 16:32:51 +0200

    .

--- src/migration.ts
+++ src/migration.ts
@@ -2,7 +2,7 @@ import { Express } from "express";
 import multer from "multer";
 import * as unzipper from "unzipper";
 import { BookmarkItems } from "./migration/kotatsu/bookmarks";
-import { BackupData } from "./model/backup";
+import { BackupData, Chapter } from "./model/backup";
 import { Categories } from "./migration/kotatsu/categories";
 import { Favourites } from "./migration/kotatsu/favourites";
 import { Histories } from "./migration/kotatsu/histories";
@@ -10,6 +10,7 @@ import { Settings } from "./migration/kotatsu/settings";
 import { Sources } from "./migration/kotatsu/sources";
 import axios from "axios";
 import { JSONPath } from "jsonpath-plus";
+import Long from "long";
 
 const upload = multer({
   storage: multer.memoryStorage(),
@@ -113,6 +114,11 @@ export function registerEndpoints(app: Express): void {
   });
 }
 
+let mangaIndex = 1;
+const mangaIds: Map<number, number> = new Map();
+let chapterIndex = 1;
+const chapterIds: Map<number, number> = new Map();
+
 function processBookmarks(backup: BackupData, bookmarks: BookmarkItems, droppedMangas: number[]) { }
 
 function processCategories(backup: BackupData, categories: Categories) {
@@ -129,12 +135,14 @@ async function processFavourites(backup: BackupData, favourites: Favourites, dro
   for (var i = 0; i < favourites.length; i++) {
     const manga = favourites[i];
     if (manga.manga.source !== "MANGADEX") {
-      console.log("Manga dropped (unknown source):", manga.manga.title);
+      console.log("Manga dropped (unknown source):", manga.manga.title, " | ", manga.manga.source);
       droppedMangas.push(manga.manga_id);
       continue;
     }
+    const mangaId = mangaIndex++;
+    mangaIds.set(manga.manga_id, mangaId);
     backup.manga.push({
-      id: manga.manga_id,
+      id: mangaId,
       categories: [manga.category_id],
       isManga: true,
       name: manga.manga.title,
@@ -155,7 +163,7 @@ async function processFavourites(backup: BackupData, favourites: Favourites, dro
       status: 0,
       source: manga.manga.source,
     });
-    const res = await paginatedChapterListRequest(manga.manga.url, 0);
+    const res = await paginatedChapterListRequest(manga.manga.url, 0, 'en');
     if (res.status === 200) {
       const chapterList = JSONPath({
         path: "$.data[*]",
@@ -173,10 +181,16 @@ async function processFavourites(backup: BackupData, favourites: Favourites, dro
         path: "$.total",
         json: res.data,
       });
+      console.log("DEBUG:", manga.manga.title);
+      getChapters(backup, chapterList, mangaId);
+      break;
       let hasMoreResults = (limit + offset) < total;
       while (hasMoreResults) {
         offset += limit;
-        var newRequest = await paginatedChapterListRequest(manga.manga.url, offset);
+        var newRequest = await paginatedChapterListRequest(manga.manga.url, offset, 'en');
+        if (newRequest.status !== 200) {
+          break;
+        }
         const total = JSONPath({
           path: "$.total",
           json: res.data,
@@ -185,15 +199,85 @@ async function processFavourites(backup: BackupData, favourites: Favourites, dro
           path: "$.data[*]",
           json: res.data,
         });
+        getChapters(backup, chapterList, mangaId);
         hasMoreResults = (limit + offset) < total;
       }
     }
   }
 }
 
-async function paginatedChapterListRequest(mangaId: string, offset: number) {
-  const res = await axios.get(`https://api.mangadex.org/manga/${mangaId}/feed?limit=500&offset=${offset}&includes[]=user&includes[]=scanlation_group&order[volume]=desc&order[chapter]=desc&translatedLanguage[]=$lang&includeFuturePublishAt=0&includeEmptyPages=0`);
+async function paginatedChapterListRequest(mangaId: string, offset: number, lang: string) {
+  const res = await axios.get(`https://api.mangadex.org/manga/${mangaId}/feed?limit=500&offset=${offset}&includes[]=user&includes[]=scanlation_group&order[volume]=desc&order[chapter]=desc&translatedLanguage[]=${lang}&includeFuturePublishAt=0&includeEmptyPages=0`);
   return res;
+}
+
+function getChapters(backup: BackupData, chapterList: any, mangaId: number) {
+  for (const chapter of chapterList) {
+    let scan = "";
+    let chapterName = "";
+    const groupName = JSONPath({ path: '$.relationships[*].attributes.name', json: chapter });
+    const uploaderName = JSONPath({ path: '$.relationships[*].attributes.username', json: chapter });
+    if (groupName?.length > 0 && groupName[0]) {
+      scan += groupName[0];
+      if (uploaderName?.length > 0 && uploaderName[0]) {
+        scan += ` Uploaded by ${uploaderName[0]}`;
+      }
+    } else {
+      scan = "No Group";
+    }
+    const volume = JSONPath({ path: '$.attributes.volume', json: chapter });
+    const chapterNum = JSONPath({ path: '$.attributes.chapter', json: chapter });
+    const title = JSONPath({ path: '$.attributes.title', json: chapter });
+    const publishDate = JSONPath({ path: '$.attributes.publishAt', json: chapter })[0];
+    const chapterId = JSONPath({ path: '$.id', json: chapter })[0];
+    if (volume?.length > 0 && volume[0] && volume[0] !== "null") {
+      chapterName = `Vol.${volume[0]} `;
+    }
+    if (chapterNum?.length > 0 && chapterNum[0] && chapterNum[0] !== "null") {
+      chapterName += `Ch.${chapterNum[0]} `;
+    }
+    if (title?.length > 0 && title[0] && title !== "null") {
+      if (chapterName.length > 0) {
+        chapterName += "- ";
+      }
+      chapterName += title[0];
+    }
+    if (chapterName.length === 0) {
+      chapterName += "Oneshot";
+    }
+
+    console.log("CHAPTER:", scan, chapterName, publishDate, chapterId);
+    const chapterUid = generateUid("MANGADEX", chapterId);
+    console.log("DEBUG:", chapterId, String(chapterUid));
+
+    const chapterIdx = chapterIndex++;
+    chapterIds.set(chapterId, chapterIdx);
+
+    backup.chapters.push({
+      id: chapterIdx,
+      url: chapterId,
+      dateUpload: publishDate,
+      isBookmarked: false,
+      scanlator: scan,
+      name: chapterName,
+      isRead: true,
+      lastPageRead: "1",
+      mangaId: mangaId,
+      archivePath: "",
+    });
+    break;
+  }
+}
+
+function generateUid(source: string, url: string): String {
+  var h = new Long(1125899906842597);
+  for (let i = 0; i < source.length; i++) {
+    h = h.multiply(31).add(source.codePointAt(i) ?? 0);
+  }
+  for (let i = 0; i < url.length; i++) {
+    h = h.multiply(31).add(url.codePointAt(i) ?? 0);
+  }
+	return h.toString(10); // check unsigned
 }
 
 function processHistories(backup: BackupData, histories: Histories, droppedMangas: number[]) {
@@ -205,8 +289,8 @@ function processHistories(backup: BackupData, histories: Histories, droppedManga
     backup.history.push({
       id: i + 1,
       isManga: true,
-      mangaId: history.manga_id,
-      chapterId: history.chapter_id,
+      mangaId: mangaIds.get(history.manga_id) ?? history.manga_id,
+      chapterId: chapterIds.get(history.chapter_id) ?? history.chapter_id,
       date: String(history.updated_at),
     });
   }
